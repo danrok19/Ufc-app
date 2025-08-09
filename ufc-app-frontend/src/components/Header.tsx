adfc5c302976619e351eaf269422d299b4268664
Kobe <danrok19@wp.pl>
2025-08-09 18:22:26 +0200

    Header

--- ufc-app-frontend/src/components/Header.tsx
+++ ufc-app-frontend/src/components/Header.tsx
@@ -1,7 +1,32 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { VscSearch } from 'react-icons/vsc';
 
 export default function Header(){
-    return <div>
-        <Link to="/">UFC APP</Link>
+
+    const [term, setTerm] = useState("")
+    const navigate = useNavigate()
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        
+        console.log(term)
+        navigate("/search")
+        setTerm('')
+    }
+
+    return <div className="border-b-5 border-yellow-500 flex items-center justify-between bg-black">
+        <Link to="/" className="text-4xl font-semibold mx-5 my-2 text-white italic">UFC APP</Link>
+        <form onSubmit={handleSubmit}>
+            <div className="border-2 border-white rounded-md py-1 mx-5 flex justify-center items-center">
+                <div className="mx-2 text-white">
+                    <VscSearch />
+                </div>
+                <input className="text-white placeholder-gray"
+                    value={term}
+                    onChange={(e) => setTerm(e.target.value)}
+                    placeholder="Find whatcha want"/>
+            </div>
+        </form>
     </div>
 }
