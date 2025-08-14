7e89be7ef2d0f3f7cb091075353bbda74c1af003
Kobe <danrok19@wp.pl>
2025-08-14 16:42:41 +0200

    homePage displayin ranking

--- ufc-app-frontend/src/pages/HomePage.tsx
+++ ufc-app-frontend/src/pages/HomePage.tsx
@@ -1,22 +1,98 @@
 import { useLoaderData } from "react-router-dom"
 import type { RankingResult } from "../api/types/rankingResult"
+import { homeQuery } from "../api/queries/homeQuery"
+import { useEffect, useState } from "react"
+import { RiMedalLine } from "react-icons/ri";
+import { BsFillPersonFill } from "react-icons/bs";
+import { CiSquareMore } from "react-icons/ci";
+
+interface ClassWeightNav{
+    key: string, 
+    value: string
+}
 
 export default function HomePage(){
-    const data: RankingResult[] = useLoaderData()
+    //const data: RankingResult[] = useLoaderData()
+    const [data, setData] = useState<RankingResult[]>(useLoaderData())
+    const [selectedClass, setSelectedClass] = useState("Lightweight")
+    const [countDisplay, setCountDisplay] = useState(5)
 
-    const { champion, weight_class, contenders } = data[0]
+    const handleClassChange = (value: string) => {
+        setSelectedClass(value)
+    }
 
-    const content = Object.values(contenders).map((fighter: string) =>{
-        return <div key={fighter}>
-            {fighter}
-        </div>
+    const handleMoreDisplay = () => {
+        setCountDisplay(15)
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response: RankingResult[] = await homeQuery(selectedClass);
+                setData(response);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, [selectedClass])
+    console.log(selectedClass)
+
+    const { champion, weight_class, contenders } = data![0]
+
+    const content = Object.values(contenders).map((fighter: string, index: number) =>{
+        if (index + 1 <= countDisplay) {
+            return <div
+                key={fighter}
+                className="border text-black mx-5 my-1 rounded p-1 bg-gray-200 cursor-pointer transition duration-200 hover:scale-110">
+                {index + 1}. {fighter}
+            </div>
+        }
+        else {
+            return null;
+        }
+    })
+
+    const classes: ClassWeightNav[] = [{key: "1", value: "Flyweight"}, {key: "2", value: "Lightweight"}, {key: "3", value: "Welterweight"}, {key: "4", value: "Middleweight"}, {key: "5", value: "Heavyweight"}]
+
+    const weightClassButtons = classes.map(c => {
+        return <button 
+                key={c.key} 
+                className="bg-gray-200 rounded px-3 py-1 transition duration-200 ease-in-out hover:bg-gray-400 cursor-pointer"
+                onClick={() => handleClassChange(c.value)}>
+                {c.value}
+            </button>
     })
 
     console.log(data)
     return <div>
         Home Page
-        {champion}
-        {weight_class}
-        {content}
+        <div className="flex gap-3 justify-center">
+            {weightClassButtons}
+        </div>
+        <div className="flex justify-center text-white text-5xl mt-8">
+            {weight_class}
+        </div>
+        <div className="flex justify-center gap-3 text-white text-2xl my-5">
+            <div className="flex justify-center items-center gap-2 cursor-pointer">
+                <RiMedalLine className="text-yellow-500" />
+                <p className="text-4xl">{champion}</p>
+            </div>
+            <div className='flex border p-5 rounded'>
+                <BsFillPersonFill className='text-3xl' />
+            </div>
+        </div>
+        <div className="flex flex-col justify-center m-auto w-80">
+            <p className="text-white text-4xl border-b text-center mb-2">Contenders</p>
+            {content}
+            {countDisplay == 5 ? <div className="flex justify-center">
+                <button
+                    className="text-white text-4xl transition duration-200 ease-in-out hover:scale-110 cursor-pointer"
+                    onClick={handleMoreDisplay}>
+                    <CiSquareMore />
+                </button>
+            </div> : <></>}
+        </div>
     </div>
 }
