911fb5c11dc223352f90eadc0b1ee1f4c6a38a07
Kobe <danrok19@wp.pl>
2025-08-14 12:31:32 +0200

    bug fix with the color in the chart

--- ufc-app-frontend/src/components/Chart.tsx
+++ ufc-app-frontend/src/components/Chart.tsx
@@ -1,8 +1,13 @@
+const colorClasses: Record<string, string> = {
+  red: "bg-red-500",
+  green: "bg-green-500"
+};
+
 export default function Chart({statName, statValue, chartColor} : {statName: string, statValue: string, chartColor: string}) {
     return <div>
         {statName}
         <div className='border py-1 pl-1 flex h-8'>
-            <div className={`bg-${chartColor}-500 h-full flex justify-center items-center`}
+            <div className={`${colorClasses[chartColor]} h-full flex justify-center items-center`}
                 style={{ width: statValue! }} >
             </div>
             <div className='flex items-center px-2'>
